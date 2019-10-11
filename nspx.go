a8f39004ceb64542cd73ba7d8c4c40a8d0cef5ad
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-11 22:07:22 +0500

    .

---
+++ nspx.go
@@ -0,0 +1,14 @@
+package nspx
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting ns-core...")
+}
+
+// ImportCheck for nspx
+func ImportCheck() {
+	fmt.Println("Nerdspex core imported.")
+}
